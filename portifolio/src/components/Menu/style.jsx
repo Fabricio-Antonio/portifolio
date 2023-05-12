00e49bf8211e1d45a96804b7fc3703513dfe35c3
Fabricio-Antonio <fabricio.ss2117@gmail.com>
2023-05-12 20:12:59 -0300

    Estilo do menu criado

--- portifolio/src/components/Menu/style.jsx
+++ portifolio/src/components/Menu/style.jsx
@@ -3,19 +3,59 @@ import styled from 'styled-components'
 export const StyledMenu = styled.div`
     height: 10vh;
     width: 100vw;
-    color: purple;
-    background-color: #7f7fa5;
+    background-color: #181818;
+    margin-bottom: 50px;
 
     ul {
-        text-decoration: none;
-        color: red;
         display: flex;
+        background-color: #181818;
         justify-content: space-between;
+        align-items: center;
+        padding: 0.5rem 6rem;
+        list-style: none;
+
     }
 
     li {
         list-style-type: none;
-        
+        text-decoration: none;
+        font-family: montserrat;
+        font-size: large;
+        margin: 10px;
+
+    }
+
+    & a{
+        position: relative;
+        color: #fff;
+        text-decoration: none;
+    }
+
+    & li a:hover{
+    color: #16B8F3;
+    transition: 0.4s;
+    }
+
+    & li a:before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 4px;
+    bottom: 0;
+    top: 20px;
+    left: 0;
+    background-color: #16B8F3;
+    visibility: hidden;
+    -webkit-transform: scaleX(0);
+    transform: scaleX(0);
+    -webkit-transition: all 0.3s ease-in-out 0s;
+    transition: all 0.3s ease-in-out 0s;
+    }
+
+    & a:hover:before {
+    visibility: visible;
+    -webkit-transform: scaleX(1);
+    transform: scaleX(1);
     }
 `;
 
