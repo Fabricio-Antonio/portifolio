4e104dddde91cc6fc0b015bb30ccc06803789d8d
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2023-10-03 21:33:35 -0300

    :ok_hand: style: Alteração no scroll lateral

--- portifolio/src/styles/global.tsx
+++ portifolio/src/styles/global.tsx
@@ -6,7 +6,19 @@ const GlobalSyles = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
         font-family: montserrat, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        
+        ::-webkit-scrollbar {
+            width: 10px;
+        }
+    
+        ::-webkit-scrollbar-track {
+            background-color: #f8f8f8;
+        }
+    
+        ::-webkit-scrollbar-thumb {
+            background-color: #16B8F3;
+            border-radius: 10px;
+            border: 2px solid #f8f8f8;
+        }
     }
 `;
 
