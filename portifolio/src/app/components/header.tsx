3d29b3f9d01de4c281360fbd7d6a868014f7f19d
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2023-11-21 18:45:54 -0300

    :sparkles: feat: Criado menu desktop

--- portifolio/src/app/components/header.tsx
+++ portifolio/src/app/components/header.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
 import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "@/components/ui/sheet";
 import { Home, MenuIcon, FolderGit2, Code2, User2, LibraryBig } from "lucide-react";
 import Link from "next/link";
@@ -7,7 +8,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <>
-            <div className="bg-backgroundblack w-full">
+            <div className="bg-backgroundblack w-full flex justify-between lg:hidden">
                 <Sheet>
                     <SheetTrigger className="py-3 px-3 ">
                         <MenuIcon className="text-backgroundwhite" />
@@ -65,7 +66,72 @@ export default function Header() {
                         </div>
                     </SheetContent>
                 </Sheet>
+                <div>
+                    
+                </div>
+                <Avatar className="w-14 m-3">
+                    <AvatarImage src="/logo.png" />
+                    <AvatarFallback>FS</AvatarFallback>
+                </Avatar>
             </div>
+            <div className="bg-backgroundblack text-backgroundwhite w-full h-16 hidden lg:flex justify-around ">
+            <div>
+                <Avatar className="w-14 flex mt-3">
+                    <AvatarImage src="/logo.png" />
+                    <AvatarFallback>FS</AvatarFallback>
+                </Avatar>
+            </div>
+                <NavigationMenu>
+                    <NavigationMenuList className=" flex gap-8">
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuItem className="bg-background cursor-pointer flex gap-1 p-2 rounded-xl hover:bg-backgroundwhite hover:text-background">
+                                <Home />
+                                <NavigationMenuLink>
+                                    Iníco
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                        </Link>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuItem className="bg-background cursor-pointer flex gap-1 p-2 rounded-xl hover:bg-backgroundwhite hover:text-background">
+                                <Code2 />
+
+                                <NavigationMenuLink>
+                                    Habilidades
+                                </NavigationMenuLink>
+
+                            </NavigationMenuItem>
+                        </Link>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuItem className="bg-background cursor-pointer flex gap-1 p-2 rounded-xl hover:bg-backgroundwhite hover:text-background">
+                                <FolderGit2 />
+
+                                <NavigationMenuLink>
+                                    Projetos
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                        </Link>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuItem className="bg-background cursor-pointer flex gap-1 p-2 rounded-xl hover:bg-backgroundwhite hover:text-background">
+                                <User2 />
+
+                                <NavigationMenuLink>
+                                    Sobre
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                        </Link>
+                        <Link href="/docs" legacyBehavior passHref>
+                            <NavigationMenuItem className="bg-background cursor-pointer flex gap-1 p-2 rounded-xl hover:bg-backgroundwhite hover:text-background">
+                                <LibraryBig />
+                                <NavigationMenuLink>
+                                    Certificados
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                        </Link>
+                    </NavigationMenuList>
+
+                </NavigationMenu>
+
+            </div >
 
         </>
     )
