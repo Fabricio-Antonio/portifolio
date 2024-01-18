d3ea453350e343b96ad373d6e653d8d1a322c006
Fabrício Antõnio <fabricio.ss2117@gmail.com>
2024-01-18 18:28:45 -0300

    :wrench: chore: Novo projeto adicionado no carrossel de clientes

--- portifolio/src/app/components/sliderprojectclient.tsx
+++ portifolio/src/app/components/sliderprojectclient.tsx
@@ -20,7 +20,7 @@ export default function SliderClients() {
     })
     return (
         <>
-            <div ref={sliderRef} className="keen-slider flex overflow-auto object-cover overflow-y-hidden justify-center">
+            <div ref={sliderRef} className="keen-slider flex overflow-auto object-cover overflow-y-hidden lg:justify-center">
                 <div className="keen-slider__slide h-[300px] rounded-xl bg-backgroundwhite border-2 border-backgroundblack">
                     <Image src='/junior.png' width={400} height={400} alt="projeto pokedex" />
                     <h3 className="text-backgroundblack font-semibold text-lg my-2">Site portifólio - Filmmeker</h3>
@@ -64,6 +64,56 @@ export default function SliderClients() {
                         </div>
                         <div>
                             <Link href="https://site-junior-ribeiro.vercel.app/"  target="_blank">
+                                <Button className="bg-background rounded-xl hover:bg-backgroundblack hover:text-backgroundwhite">
+                                    Visitar
+                                </Button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="keen-slider__slide h-[300px] rounded-xl bg-backgroundwhite border-2 border-backgroundblack">
+                    <Image src='/mermake.png' width={400} height={400} alt="projeto pokedex" />
+                    <h3 className="text-backgroundblack font-semibold text-lg my-2">E-commerce - Mermake Santiago</h3>
+                    <div className="flex justify-center gap-1 lg:gap-5">
+                        <div>
+                            <Dialog>
+                                <DialogTrigger asChild>
+                                    <Button variant="outline" className="rounded-xl border-none hover:bg-backgroundblack hover:text-backgroundwhite">Saiba mais</Button>
+                                </DialogTrigger>
+                                <DialogContent className="sm:max-w-[425px] rounded-xl ">
+                                    <DialogHeader>
+                                        <DialogTitle>E-commerce - Mermake Santiago</DialogTitle>
+                                        <DialogDescription>
+                                            <span className="uppercase font-semibold">tecnologias</span> <br />
+                                            ReactJS, TypeScript, TailwindCSS <br /><br />
+                                            Já imaginou seu negócio na mão do seu cliente, a venda a um toque de distância? Um E-comerce desempenham um papel significativo na modernização do comércio, proporcionando conveniência aos consumidores e criando oportunidades de negócios para empreendedores e empresas de todos os tamanhos. Eficiente e personalizado, atendendo às demandas assim possibilitando que você alcance um público mais amplo.
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                    <div className="grid gap-4 py-4">
+                                        <div className="grid grid-cols-4 items-center gap-4">
+                                            <div className="grid grid-cols-4 items-center gap-4">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <DialogFooter>
+                                        <div className="flex justify-center gap-3">
+                                            <Link href="https://mermake-santiago-store.vercel.app/"  target="_blank">
+                                                <Button className="bg-background rounded-xl hover:bg-backgroundblack hover:text-backgroundwhite">
+                                                    Visitar
+                                                </Button>
+                                            </Link>
+                                            <Link href="https://api.whatsapp.com/send?phone=5581983587510&text=Oi%2C%20Fabr%C3%ADcio.%20Eu%20vi%20seu%20portif%C3%B3lio%20e%20gostei%20muito%20do%20seu%20trabalho%2C%20gostaria%20de%20conversar%20contigo."  target="_blank">
+                                                <Button className="bg-background rounded-xl hover:bg-backgroundblack hover:text-backgroundwhite">
+                                                    Solicitar projeto
+                                                </Button>
+                                            </Link>
+                                        </div>
+                                    </DialogFooter>
+                                </DialogContent>
+                            </Dialog>
+                        </div>
+                        <div>
+                            <Link href="https://mermake-santiago-store.vercel.app/"  target="_blank">
                                 <Button className="bg-background rounded-xl hover:bg-backgroundblack hover:text-backgroundwhite">
                                     Visitar
                                 </Button>
